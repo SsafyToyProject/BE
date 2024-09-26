61cb9f452c847e51e4cd0b760cd758d8582c0e3a
dajeong <rlaekwjd6566@gmail.com>
2024-09-26 09:52:13 +0900

    style: update comment

--- MockCoteServer/src/main/java/model/dto/StudyDto.java
+++ MockCoteServer/src/main/java/model/dto/StudyDto.java
@@ -3,13 +3,13 @@ package model.dto;
 import java.util.List;
 
 public class StudyDto {
-	private int study_id;
-	private int owner_id;
-	private String name;
-	private String description;
-	private String code;
+	private int study_id; //스터디 고유 ID
+	private int owner_id; //스터니 owner ID
+	private String name; //스터디 이름
+	private String description; //스터디 설명
+	private String code;// 스터디 live-coding 참가 url 등을 위한 정보 코드
 	
-	private List<UserDto> studyMambers;
+	private List<UserDto> studyMambers; //해당 스터디에 참여하는 스터디원 
 	
 	public int getStudy_id() {
 		return study_id;
--- MockCoteServer/src/main/java/model/dto/UserDto.java
+++ MockCoteServer/src/main/java/model/dto/UserDto.java
@@ -1,10 +1,21 @@
 package model.dto;
 
 public class UserDto {
-	private int user_id;
-	private String handle;
-	private String password;
-	private int level;
+	private int user_id; //사용자 고유 ID
+	private String handle; //백준 handle (백준 ID)
+	private String password; //비밀번호 
+	private int level; //백준 티어
+	
+	
+	
+	public UserDto(int user_id, String handle, String password, int level) {
+		super();
+		this.user_id = user_id;
+		this.handle = handle;
+		this.password = password;
+		this.level = level;
+	}
+	
 	public int getUser_id() {
 		return user_id;
 	}
