0eb201d21238c020dcb3b915a3d0bb08c208fc3e
phs7646 <phs7646@kaist.ac.kr>
2024-09-30 16:48:53 +0900

    Feat: Problem Dao, Query Dao interface 완성

--- MockCoteServer/src/main/java/crawler/CrawlerDao.java
+++ MockCoteServer/src/main/java/crawler/CrawlerDao.java
@@ -1,5 +1,7 @@
 package crawler;
 
+//deprecated
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
--- MockCoteServer/src/main/java/model/dao/QueryDao.java
+++ MockCoteServer/src/main/java/model/dao/QueryDao.java
@@ -1,5 +1,29 @@
 package model.dao;
 
-public interface QueryDao {
+import java.util.List;
 
+import model.dto.QueryDto;
+
+public interface QueryDao {
+	
+	/**
+	 * 쿼리 객체를 DB에 삽입합니다.
+	 * @param query
+	 */
+	void insertQuery(QueryDto query);
+	
+	/**
+	 * 모든 쿼리객체를 반환합니다.
+	 * @return QueryDto List
+	 */
+	List<QueryDto> searchAll();
+	
+	/**
+	 * query_id에 해당하는 쿼리 객체를 반환합니다
+	 * @param query_id
+	 * @return queryDto
+	 */
+	QueryDto searchById(int query_id);
+	
+	
 }
