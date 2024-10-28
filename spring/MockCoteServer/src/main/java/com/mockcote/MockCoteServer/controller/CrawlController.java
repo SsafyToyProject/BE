d51ae35b6640db0bef652f4aca4aa690c2a800fc
phs7646 <phs7646@kaist.ac.kr>
2024-10-28 16:39:20 +0900

    피드백 반영

--- spring/MockCoteServer/src/main/java/com/mockcote/MockCoteServer/controller/CrawlController.java
+++ spring/MockCoteServer/src/main/java/com/mockcote/MockCoteServer/controller/CrawlController.java
@@ -4,8 +4,8 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -43,7 +43,7 @@ public class CrawlController {
     	Map<String, Object> response = new LinkedHashMap<>();
     	response.put("query_id", created.getQueryId());
     	response.put("num_problems", created.getNumProblems());
-    	return ResponseEntity.ok(response);
+    	return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
     
     
