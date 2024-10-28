09562cf23b57059a50779acc33e87fe4b0dfa4f3
김정우 <3957ki@gmail.com>
2024-10-28 15:11:32 +0900

    스터디 생성 및 특정 스터디 조회 추가

--- spring/MockCoteServer/src/main/java/com/mockcote/MockCoteServer/model/mapper/StudyMapper.java
+++ spring/MockCoteServer/src/main/java/com/mockcote/MockCoteServer/model/mapper/StudyMapper.java
@@ -1,29 +1,35 @@
 package com.mockcote.MockCoteServer.model.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Param;
 
 import com.mockcote.MockCoteServer.dto.Study;
+import com.mockcote.MockCoteServer.dto.User;
 
 public interface StudyMapper {
-	
-	/*
-	 * // 스터디 추가 int addStudy(Study studyDto);
-	 * 
-	 * // 생성된 스터디ID인 스터디 멤버에 방장 추가 int addStudyIdAndOwnerId(@Param("studyId") int
-	 * studyId, @Param("ownerId") int ownerId);
-	 * 
-	 * // 스터디ID로 상세 조회 Study getStudyById(int studyId);
-	 * 
-	 * // 스터디ID로 스터디 멤버 리스트 받아오기 List<User> getUsersByStudyId(int studyId);
-	 */
-	
-//	code로 스터디 정보 조회
+
+
+	// 스터디 추가
+	int addStudy(Study studyDto);
+
+	// 생성된 스터디ID인 스터디 멤버에 방장 추가
+	int addStudyIdAndOwnerId(@Param("studyId") int studyId, @Param("ownerId") int ownerId);
+
+	// 스터디ID로 상세 조회
+	Study getStudyById(int studyId);
+
+	// 스터디ID로 스터디 멤버 리스트 받아오기
+	List<User> getUsersByStudyId(int studyId);
+
+
+	//	code로 스터디 정보 조회
 	Study getStudyByCode(String code);
-	
-//	스터디ID로 스터디 삭제하기
+
+	//	스터디ID로 스터디 삭제하기
 	int deleteStudyById(int studyId);
 
-//	특정 유저의 스터디 탈퇴
+	//	특정 유저의 스터디 탈퇴
 	int leaveStudyById(@Param("studyId") int studyId, @Param("userId") int userId);
 
 }
