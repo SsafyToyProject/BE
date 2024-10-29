3218dccd056cbc3e322e3093f2a989175c9fdaba
김다정 <rlaekwjd6566@gmail.com>
2024-10-29 10:21:43 +0900

    세션트래커 컨트롤러 에러 처리 수정

--- spring/MockCoteServer/src/main/java/com/mockcote/MockCoteServer/controller/SessionTrackerController.java
+++ spring/MockCoteServer/src/main/java/com/mockcote/MockCoteServer/controller/SessionTrackerController.java
@@ -17,23 +17,16 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/tracker")
 @RequiredArgsConstructor
 public class SessionTrackerController {
-	private final SessionTrackerService sessionTrackerService;
-	
-	 @GetMapping("/info/{session_id}/{user_id}/{problem_id}")
-	    public ResponseEntity<SessionTracker> getTrackerInfo(@PathVariable("session_id") int sessionId,
-	                                                         @PathVariable("user_id") int userId,
-	                                                         @PathVariable("problem_id") int problemId) {
-	        try {
-	            SessionTracker tracker = sessionTrackerService.getSessionTracker(sessionId, userId, problemId);
-	            if (tracker == null) {
-	                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No tracker found for the provided ids.");
-	            }
-	            return ResponseEntity.ok(tracker);
-	        } catch (NumberFormatException e) {
-	            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid ID format");
-	        } catch (Exception e) {
-	            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error occurred");
-	        }
-	    }
+    private final SessionTrackerService sessionTrackerService;
 
+    @GetMapping("/info/{session_id}/{user_id}/{problem_id}")
+    public ResponseEntity<SessionTracker> getTrackerInfo(@PathVariable("session_id") int sessionId,
+                                                         @PathVariable("user_id") int userId,
+                                                         @PathVariable("problem_id") int problemId) {
+        SessionTracker tracker = sessionTrackerService.getSessionTracker(sessionId, userId, problemId);
+        if (tracker == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No tracker found for the provided ids.");
+        }
+        return ResponseEntity.ok(tracker);
+    }
 }
